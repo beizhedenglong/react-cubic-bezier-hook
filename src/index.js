e44eda078227a378f24b6b4173758adb9fbd956e
Victor Wang(wang shaojie) <wang_shaojie@icloud.com>
2019-02-24 04:02:53 +0800

    useChain

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import BezierEasing from 'bezier-easing'
-import { useSpring, animated } from 'react-spring'
-
-function App() {
-  const props = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    immediate: true,
-  })
-  return <animated.div style={props}>I will fade in</animated.div>
-}
-
 
 const createEasingHook = (mX1, mY1, mX2, mY2) => (duration = 1000, immediate = true) => {
   const [percent, set] = useState(0)
@@ -42,56 +31,54 @@ const createEasingHook = (mX1, mY1, mX2, mY2) => (duration = 1000, immediate = t
   }, [immediateStatus])
   return { percent, setImmediate }
 }
+
 const useEasing = createEasingHook(0.25, 0.1, 0.25, 1.0)
 const useLiner = createEasingHook(0.00, 0.0, 1.00, 1.0)
 const useEaseIn = createEasingHook(0.42, 0.0, 1.00, 1.0)
 const useEaseInOut = createEasingHook(0.42, 0.0, 0.58, 1.0)
 
+const useChain = (...arr) => {
+  const res = arr.map(({ hook, args }, index) => {
+    if (index === 0) {
+      return hook(...args)
+    }
+    return hook(...args, false)
+  })
+  useEffect(() => {
+    res.forEach(({ percent }, index) => {
+      if ((percent === 1) && res[index + 1]) {
+        res[index + 1].setImmediate(true)
+      }
+    })
+  })
+  return res
+}
+
 const Test = () => {
-  const easing1 = useEasing(1000)
-  const easing2 = useLiner(1000)
-  const easing3 = useEaseIn(1000)
-  const easing4 = useEaseInOut(1000, false)
+  const hooksReturns = useChain(
+    { hook: useEasing, args: [500] },
+    { hook: useLiner, args: [1000] },
+    { hook: useEaseIn, args: [200] },
+    { hook: useEaseInOut, args: [300] },
+    { hook: useEasing, args: [400] },
+    { hook: useLiner, args: [2000] },
+  )
+  const colors = ['pink', 'yellow', 'blue', 'grey']
+  const divs = hooksReturns.map(({ percent }, index) => (
+    <div
+      key={index} // eslint-disable-line
+      style={{
+        position: 'relative',
+        left: 500 * percent,
+        width: 100,
+        height: 100,
+        background: colors[index % 4],
+      }}
+    />
+
+  ))
   return (
-    <div>
-      <App />
-      <div
-        style={{
-          position: 'relative',
-          left: 500 * easing1.percent,
-          width: 100,
-          height: 100,
-          background: 'black',
-        }}
-      />
-      <div
-        style={{
-          position: 'relative',
-          left: 500 * easing2.percent,
-          width: 100,
-          height: 100,
-          background: 'yellow',
-        }}
-      />
-      <div
-        style={{
-          position: 'relative',
-          left: 500 * easing3.percent,
-          width: 100,
-          height: 100,
-          background: 'green',
-        }}
-      />
-      <div
-        onClick={() => easing4.setImmediate(true)}
-        style={{
-          position: 'relative',
-          width: 100 + 500 * easing4.percent,
-          height: 100,
-          background: 'grey',
-        }}
-      />
-    </div>
+    divs
   )
 }
 
