0868908242ed04310d649b418774a6159f10512d
Victor Wang(wang shaojie) <wang_shaojie@icloud.com>
2019-02-24 02:36:36 +0800

    add immediate config

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
 import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import BezierEasing from 'bezier-easing'
+import { useSpring, animated } from 'react-spring'
+
+function App() {
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    immediate: true,
+  })
+  return <animated.div style={props}>I will fade in</animated.div>
+}
 
 
-const createEasingHook = (mX1, mY1, mX2, mY2) => (duration = 1000) => {
+const createEasingHook = (mX1, mY1, mX2, mY2) => (duration = 1000, immediate = true) => {
   const [percent, set] = useState(0)
   const startedRef = useRef(null)
   const bezierFn = BezierEasing(mX1, mY1, mX2, mY2)
+  const rafRef = useRef(null)
+  const [immediateStatus, setImmediate] = useState(immediate)
+  const cancelIfNecessary = () => rafRef.current && cancelAnimationFrame(rafRef.current)
   useEffect(() => {
+    if (!immediateStatus) {
+      return cancelIfNecessary
+    }
     const doAnimation = (timestamp) => {
       if (!startedRef.current) {
         startedRef.current = timestamp
       }
-      const timePassed = Date.now() - startedRef.current
-      console.log(timePassed)
+      const timePassed = timestamp - startedRef.current
       if (timePassed <= duration) {
         set(bezierFn(timePassed / duration))
-        window.requestAnimationFrame(doAnimation)
+        cancelIfNecessary()
+        rafRef.current = requestAnimationFrame(doAnimation)
       } else {
         set(bezierFn(1))
       }
     }
-    window.requestAnimationFrame(doAnimation)
-    // if (!startedRef.current) {
-    //   startedRef.current = Date.now()
-    // }
-    // const timerId = setInterval(() => {
-    //   let timePassed = Date.now() - startedRef.current
-    //   if (timePassed > duration) {
-    //     timePassed = duration
-    //     clearInterval(timerId)
-    //   }
-    //   set(bezierFn(timePassed / duration))
-    // })
-    // return () => clearInterval(timerId)
-  }, [])
-  return { percent }
+    rafRef.current = requestAnimationFrame(doAnimation)
+    return cancelIfNecessary
+  }, [immediateStatus])
+  return { percent, setImmediate }
 }
 const useEasing = createEasingHook(0.25, 0.1, 0.25, 1.0)
 const useLiner = createEasingHook(0.00, 0.0, 1.00, 1.0)
@@ -46,13 +51,14 @@ const Test = () => {
   const easing1 = useEasing(1000)
   const easing2 = useLiner(1000)
   const easing3 = useEaseIn(1000)
-  const easing4 = useEaseInOut(1000)
+  const easing4 = useEaseInOut(1000, false)
   return (
     <div>
+      <App />
       <div
         style={{
           position: 'relative',
-          left: 1000 * easing1.percent,
+          left: 500 * easing1.percent,
           width: 100,
           height: 100,
           background: 'black',
@@ -61,7 +67,7 @@ const Test = () => {
       <div
         style={{
           position: 'relative',
-          left: 1000 * easing2.percent,
+          left: 500 * easing2.percent,
           width: 100,
           height: 100,
           background: 'yellow',
@@ -70,16 +76,17 @@ const Test = () => {
       <div
         style={{
           position: 'relative',
-          left: 1000 * easing3.percent,
+          left: 500 * easing3.percent,
           width: 100,
           height: 100,
           background: 'green',
         }}
       />
       <div
+        onClick={() => easing4.setImmediate(true)}
         style={{
           position: 'relative',
-          width: 100 + 1000 * easing4.percent,
+          width: 100 + 500 * easing4.percent,
           height: 100,
           background: 'grey',
         }}
